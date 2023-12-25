da04f764fa6c76f65a3effbca05fd59a20a67a39
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2023-12-25 22:07:57 +0100

    Update singleVideoFetch.js

--- singleVideoFetch.js
+++ singleVideoFetch.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const apiKey = "AIzaSyBT8cbvJmJrceNAOrJnDQFpsk7i8lkGAYM"; // Replace with your actual API key
+const apiKey = "AMIzaSyBT8cbvJmJrceNAOrJnDQFpsk7i8lkGAYM"; // Replace with your actual API key
 const videoId = "u4NYbhjYpw8"; // Replace with the ID of the YouTube video you want to fetch
 
 const apiUrl = `https://www.googleapis.com/youtube/v3/videos?key=${apiKey}&id=${videoId}&part=snippet,contentDetails`;
